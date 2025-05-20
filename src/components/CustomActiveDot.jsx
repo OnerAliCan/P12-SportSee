61d9dc35c12d0a1173eb5d6813c0cf84cdc7939c
unknown <alican.oner.pro@gmail.com>
2025-05-20 21:28:20 +0200

    début fetch API

---
+++ src/components/CustomActiveDot.jsx
@@ -0,0 +1,7 @@
+const CustomActiveDot = (props) => {
+  const { cx, cy } = props
+  // console.log(props)
+  if (cx === undefined || cy === undefined) return null
+  return <circle cx={cx} cy={cy} r={6} fill="#FFF" stroke="none" />
+}
+export default CustomActiveDot
--- src/components/CustomTick.jsx
+++ src/components/CustomTick.jsx
@@ -1,15 +1,15 @@
-export default function CustomTick(props) {
-  const { x, y, payload } = props
-  return (
-    <text
-      x={x - 10}
-      y={y + 15}
-      fill="#fff"
-      fontSize={12}
-      opacity={0.7}
-      textAnchor="middle"
-    >
-      {payload.value}
-    </text>
-  )
-}
+// export default function CustomTick(props) {
+//   const { x, y, payload } = props
+//   return (
+//     <text
+//       x={x - 10}
+//       y={y + 15}
+//       fill="#fff"
+//       fontSize={12}
+//       opacity={0.7}
+//       textAnchor="middle"
+//     >
+//       {payload.value}
+//     </text>
+//   )
+// }
